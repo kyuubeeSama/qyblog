ea26493b433d4ce5fba851ee4b37e6bf5bad2a1b
kyuubee <yshjbw@163.com>
2019-11-27 17:43:58 +0800

    添加自定义标签，添加个人中心首页

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,10 +1,27 @@
 <?php
 namespace app\admin\controller;
 
-class Index
+use think\Controller;
+use think\Db;
+
+class Index extends Controller
 {
-    public function index()
-    {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V5.1<br/><span style="font-size:30px">12载初心不改（2006-2018） - 你值得信赖的PHP框架</span></p></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="eab4b9f840753f8e7"></think>';
-    }
+	// 后台首页
+	public function index(){
+		return view('index/index');
+	}
+	// 欢迎页面
+	public function welcome(){
+		$assign=[
+			'all_article'=>Db::table('qy_article')->count(),
+			'delete_article'=>Db::table('qy_article')->where('is_delete','1')->count(),
+			'hide_article'=>Db::table('qy_article')->where('is_show','0')->count(),
+			'all_chat'=>Db::table('qy_chat')->count(),
+			'delete_chat'=>Db::table('qy_chat')->where('is_delete','0')->count(),
+			'hide_chat'=>Db::table('qy_chat')->where('is_show','0')->count(),
+			'all_comment'=>Db::table('qy_comment')->count()
+		];
+		$this->assign($assign);
+		return view('index/welcome');
+	}
 }
---
+++ application/admin/tagLib/Mytag.php
@@ -0,0 +1,43 @@
+<?php
+namespace app\admin\tagLib;
+use think\template\taglib;
+
+class Mytag extends TagLib{
+	protected $tags = [
+		//标签定义： attr 属性列表，close 是否闭合（0或1，默认1），alias标签别名 level嵌套层次
+		'bootstrapcss' => ['','close'=>'0'],
+		'bootstrapjs'  => ['','close'=>'0'],
+	];
+
+	// 当不使用content的时候，闭合标签没有效果
+	// 修改过此文件后，需要改动下模板的内容，否则模板有缓存不会执行新的内容。
+	public function tagBootstrapcss() {
+		$link=<<<php
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<link rel="stylesheet" type="text/css" href="__PUBLIC__/bootstrap-3.3.7/css/bootstrap.min.css">
+	    <link rel="stylesheet" type="text/css" href="__PUBLIC__/bootstrap-3.3.7/css/bootstrap-theme.min.css">
+	    <link rel="stylesheet" type="text/css" href="__PUBLIC__/font-awesome-4.7.0/css/font-awesome.min.css">
+	    <link rel="stylesheet" type="text/css" href="__PUBLIC__/css/bjy.css">
+	    <link rel="stylesheet" type="text/css" href="__HOME__/css/index.css">
+php;
+		return $link;
+	}
+
+	public function tagBootstrapjs(){
+		$link=<<<php
+			<script src="__PUBLIC__/js/jquery-3.4.1.min.js"></script>
+			<script>
+			    logoutUrl="{:url('Home/User/logout')}";
+			</script>
+			<script src="__PUBLIC__/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+			<!--[if lt IE 9]>
+			<script src="__PUBLIC__/js/html5shiv.min.js"></script>
+			<script src="__PUBLIC__/js/respond.min.js"></script>
+			<![endif]-->
+			<script src="__PUBLIC__/pace/pace.min.js"></script>
+			<script src="__HOME__/js/index.js"></script>
+		php;
+		return $link;
+	}
+
+}
