3f945e81ecb14d8b39e3800955636b8eec2761e7
kyuubee <yshjbw@163.com>
2020-11-25 15:37:33 +0800

    save

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -133,6 +133,7 @@ class ComposerStaticInit76984f600eb8e1555aa2af12ad04919b
 
     public static $classMap = array (
         'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
         'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
         'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
